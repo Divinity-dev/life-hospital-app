3788e6622cbb93c06a76ad1b4301fa149cbb0927
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-07-16 12:13:09 +0100

    Add route for login

--- Server/Routes/authRoute.js
+++ Server/Routes/authRoute.js
@@ -14,9 +14,31 @@ try {
     const saveduser = await user.save();
     res.status(200).json(saveduser)
 } catch (error) {
-    console.log(err)
+    res.status(400).json(err)
 }
     
+})
+
+//LOgin
+router.post("/login", async(req,res)=>{
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+
+        const decryptedPassword = cryptojs.AES.decrypt(user.password, process.env.crypto_key).toString(cryptojs.enc.Utf8);
+        if (decryptedPassword === req.body.password) {
+            res.status(200).json(user);
+        } else {
+            res.status(401).json({ error: 'Incorrect password' });
+        }
+        
+        
+    } catch (err) {
+        res.status(401).json(err)
+    }
+   
 })
 
 export default router;
