0562a8b6d0b4f551e01cf39cc1aef00e54b826cf
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-07-25 18:20:28 +0100

    modify input form

--- client/life-hospital/src/pages/Login.jsx
+++ client/life-hospital/src/pages/Login.jsx
@@ -29,7 +29,7 @@ const handleSubmit = async (e)=>{
 
   return (
     <div className='flex justify-center items-center bg-[url(https://img.freepik.com/free-photo/health-still-life-with-copy-space_23-2148854034.jpg?t=st=1721809035~exp=1721812635~hmac=2bf7f91574c72d415705fcd241c0d5ea8976f072d81a2f79b694f9afa2aae6e3&w=1060)] object-cover  bg-cover bg-center bg-no-repeat bg-blend-overlay  from-black/100 to-black/100 '>
-<div className='flex flex-col justify-center items-left w-1/3 h-1/3 mt-52 p-4'>
+<div className='flex flex-col w-full h-full md:w-1/3 md:h-1/3 mt-40 p-4'>
       <h2 className='text-2xl font-bold mb-5'>SIGN IN</h2>
       <form action="" className='flex flex-col justify-center items-start mb-5' onSubmit={handleSubmit}>
         <input type="text" placeholder='username' className='w-full p-3 mb-5' name="username" onChange={handlechange}/>
--- client/life-hospital/src/pages/Register.jsx
+++ client/life-hospital/src/pages/Register.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
+import axios from "axios"
 
 const Register = () => {
+const [user, setUser] = useState({
+  username: '',
+  Email: '',
+  password: ''
+})
+
+const handlechange = (e)=>{
+const value = e.target.value;
+setUser({
+  ...user,
+  [e.target.name]:value
+})
+}
+
+const handleSubmit = async (e)=>{
+e.preventDefault()
+try {
+  const res = await axios.post("http://localhost:3000/api/auth/register", user)
+  setUser({
+    username: '',
+    Email: '',
+    password: ''
+  })
+} catch (error) {
+  console.log(error)
+}
+}
+
   return (
     <div className='flex justify-center items-center bg-[url(https://img.freepik.com/free-photo/health-still-life-with-copy-space_23-2148854034.jpg?t=st=1721809035~exp=1721812635~hmac=2bf7f91574c72d415705fcd241c0d5ea8976f072d81a2f79b694f9afa2aae6e3&w=1060)] object-cover  bg-cover bg-center bg-no-repeat bg-blend-overlay  from-black/100 to-black/100'>
-      <div className='flex flex-col w-1/3 h-1/3 mt-40 p-4'>
+      <div className='flex flex-col w-full h-full md:w-1/3 md:h-1/3 mt-40 p-4'>
         <h2 className='text-2xl font-bold mb-5'>
         Craete an account
         </h2>
-        <form action="" className='flex flex-col justify-center items-start mb-5'>
-          <input type="text" placeholder='Username'className='w-full p-3 mb-5' />
-          <input type="text" placeholder='Email' className='w-full p-3 mb-5' />
-          <input type="text" placeholder='password' className='w-full p-3 mb-5' />
+        <form action="" className='flex flex-col justify-center items-start mb-5' onSubmit={handleSubmit}>
+          <input type="text" placeholder='Username'className='w-full p-3 mb-5' name='username' onChange={handlechange}/>
+          <input type="text" placeholder='Email' className='w-full p-3 mb-5' name='Email' onChange={handlechange} />
+          <input type="text" placeholder='password' className='w-full p-3 mb-5' name='password' onChange={handlechange} />
           <button className='bg-green-950 p-2 text-white cursor-pointer w-40'>register</button>
         </form>
         <Link to="/login" className='text-white font-bold'>Log in</Link>
