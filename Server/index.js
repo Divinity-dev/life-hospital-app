6a401a531ff8adb570dbc5f0289b89c4d5e2bf69
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-08-22 00:11:07 +0100

    fix backend

--- Server/index.js
+++ Server/index.js
@@ -24,16 +24,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
+
+app.use(express.json({ limit: '50mb' }));
+app.use(cors())
+app.use(express.json())
+
 app.use(express.static(join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(join(__dirname, 'client/build/index.html'));
 });
-app.use(express.json({ limit: '50mb' }));
-app.use(cors())
-app.use(express.json())
-
-
 app.get('/', (req, res) => {
     res.send('Backend is up and running!');
 });
