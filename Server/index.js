57707d8b241a76ce963e64e7bdf17256628d9fa1
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-08-22 10:39:49 +0100

    correct path

--- Server/index.js
+++ Server/index.js
@@ -28,10 +28,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static(join(__dirname, 'client\life-hospital\public/')));
+app.use(express.static(join(__dirname, 'client/life-hospital/public/')));
 
 app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, 'client\life-hospital\public/index.html'));
+    res.sendFile(join(__dirname, 'client/life-hospital/public/index.html'));
 });
 app.get('/', (req, res) => {
     res.send('Backend is up and running!');
