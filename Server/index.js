9c7a857154e9bd75e7019a4491737b97966e414f
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-08-22 11:30:30 +0100

    fix path issue

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ import Comments from "./Routes/Comments.js"
 import multer from 'multer';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-
+import fs from 'fs';
 
 dotenv.config()
 mongoose.connect(process.env.Mongo_url).then(()=>{
@@ -30,11 +30,11 @@ const __dirname = dirname(__filename);
 app.use(express.json({ limit: '50mb' }));
 app.use(cors())
 app.use(express.json())
-
-app.use(express.static(join(__dirname, '../client/life-hospital/public')));
+const staticPath = join(__dirname, '../client/life-hospital/public');
+app.use(express.static(staticPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '../client/life-hospital/public/index.html'));
+    res.sendFile(join(staticPath,'index.html'));
 });
 app.get('/', (req, res) => {
     res.send('Backend is up and running!');
