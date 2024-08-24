a33e1979618bebef3b4c67baa20dda5c56f26efc
Divinity-dev <divine_asiriuwa@yahoo.com>
2024-08-24 13:09:04 +0100

    remove static file rendering

--- Server/index.js
+++ Server/index.js
@@ -9,9 +9,7 @@ import LikeRoute from "./Routes/LikeRoute.js"
 import BookingRouthe from "./Routes/BookingRouthe.js"
 import Comments from "./Routes/Comments.js"
 import multer from 'multer';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import fs from 'fs';
+
 
 dotenv.config()
 mongoose.connect(process.env.Mongo_url).then(()=>{
@@ -23,19 +21,11 @@ mongoose.connect(process.env.Mongo_url).then(()=>{
 const app = express();
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 
 app.use(express.json({ limit: '50mb' }));
 app.use(cors())
 app.use(express.json())
-const staticPath = join(__dirname, '../client/life-hospital/public');
-app.use(express.static(staticPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(join(staticPath,'index.html'));
-});
 app.get('/', (req, res) => {
     res.send('Backend is up and running!');
 });
